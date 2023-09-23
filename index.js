1baae8ba29098cc2006e1b619de8949788374395
AtifxUMTS <atifalishahnaqvi3345@gmail.com>
2023-09-23 11:01:46 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+"use strict";
+//Allama Iqbal said From your past emerges the present, and from the present is born your future.
+console.log('Allama Iqbal said From your past emerges the present, and from the present is born your future.');
---
+++ index.ts
@@ -0,0 +1,4 @@
+//famous Quote: find a quote from famous person you admire. print the quote and the name of its author. your output should look.
+
+//Allama Iqbal said From your past emerges the present, and from the present is born your future.
+console.log('Allama Iqbal said From your past emerges the present, and from the present is born your future.')
